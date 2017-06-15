ec19f6d5069797bdcf24d631a0ad3e83beff7bc2
Samuel Mercado <samuel.mg@gmx.com>
2017-06-15 18:14:43 -0500

    Aplica PSR-2

--- src/Nat/Nat.php
+++ src/Nat/Nat.php
@@ -2,8 +2,8 @@
 
 namespace Nat;
 
-class Nat {
-
+class Nat
+{
     private $simple;
     private $compuesto;
     private $centenas;
@@ -20,16 +20,16 @@ class Nat {
         $this->numero = $numero;
         $this->numero_entero = (integer)$numero;
 
-        if(empty($moneda)){
+        if (empty($moneda)) {
             $this->moneda = "PESOS";
         } else {
             $this->moneda = strtoupper($moneda);
         }
 
-        $this->set_variables ();
+        $this->set_variables();
     }
 
-    private function set_variables ()
+    private function set_variables()
     {
         $this->simple['0']='CERO';
         $this->simple['1']='UN';
@@ -91,12 +91,12 @@ class Nat {
     private function menor_cien($ud)
     {
         //elige menores a 20 y multiplos de 10
-        if ($ud == 0 ) {
+        if ($ud == 0) {
             $ud_txt = '';
-        } elseif ( ($ud < 20) || ($ud%10 == 0) ) {
+        } elseif (($ud < 20) || ($ud%10 == 0)) {
             $ud_txt = $this->simple[$ud];
         } else {
-            $ud_txt = $this->compuesto[$ud/10].$this->simple[$ud%10];
+            $ud_txt = $this->compuesto[$ud/10] . $this->simple[$ud%10];
         }
         return $ud_txt;
     }
@@ -104,7 +104,7 @@ class Nat {
     //Convierte numeros entre 99 y 100(Centenas)
     private function convertir_centenas($c)
     {
-        if ( $c == 100 ) {
+        if ($c == 100) {
             $c_txt = $this->centenas['0'];
         } else {
             $c_txt = $this->centenas[$c/100];
@@ -114,13 +114,13 @@ class Nat {
 
     private function menor_mil($n)
     {
-        if( $n < 100 ) {
+        if ($n < 100) {
             $n_txt = $this->menor_cien($n);
-        } elseif ( $n < 1000 ) {
+        } elseif ($n < 1000) {
             $ud = $n % 100;
             $ud_txt = $this->menor_cien($ud);
             $c_txt = $this->convertir_centenas($n);
-            $n_txt = $c_txt.$ud_txt;
+            $n_txt = $c_txt . $ud_txt;
         }
         return $n_txt;
     }
@@ -130,29 +130,26 @@ class Nat {
     {
         $n_str = (string)$this->numero_entero;//Convierte el número entero a cadena
 
-        if($this->numero_entero == 0){
+        if ($this->numero_entero == 0) {
             $letras = $this->simple[0] . " " . $this->moneda;
         }
 
         //Numero < 1000
-        elseif ( $this->numero_entero < 1000 )
-        {
+        elseif ($this->numero_entero < 1000) {
             $n = $this->numero_entero;
             $n_txt = $this->menor_mil($n);
-            $letras = $n_txt." ".$this->moneda;
+            $letras = $n_txt . " " . $this->moneda;
         }
 
         //Numero {1000 < n < 1,000,000}
-        elseif ( $this->numero_entero < 1000000 )
-        {
+        elseif ($this->numero_entero < 1000000) {
             $m = 0;
-            $long_n = strlen ($n_str);//cuenta el número de caracteres
+            $long_n = strlen($n_str);//cuenta el número de caracteres
             $f = 1;
 
             //Separa los miles para ser evaluados con la función menor_mil
-            for ($k = $long_n; $k > 3; $k--)
-            {
-                $m += ( $n_str[$k-4] * $f );	//recupera la posicion 0,1,2 y Multiplica por 1,10,y 100
+            for ($k = $long_n; $k > 3; $k--) {
+                $m += ($n_str[$k-4] * $f);    //recupera la posicion 0,1,2 y Multiplica por 1,10,y 100
                 $f *= 10;
             }
 
@@ -162,23 +159,20 @@ class Nat {
             $n = $this->numero_entero - $m * 1000;//Elimina los miles
 
             $n_txt = $this->menor_mil($n);//Unidades, Decenas y Centenas a ser evaluadas
-            $letras = $miles_txt.$this->miles.$n_txt." ".$this->moneda;
+            $letras = $miles_txt . $this->miles . $n_txt . " " . $this->moneda;
         }
 
         //número mayor o igual a 1,000,000
-        else
-        {
+        else {
             $long_n = strlen($n_str);//cuenta el número de caracteres
             $f = 1;
             $mm = 0;
-            for ($k = $long_n; $k > 6; $k--)
-            {
-                $mm += ($n_str[$k-7] * $f);	//Recupera la posición 0,1,2 del array u la Multiplica por 1,10,y 100 respectivamente
+            for ($k = $long_n; $k > 6; $k--) {
+                $mm += ($n_str[$k-7] * $f);    //Recupera la posición 0,1,2 del array u la Multiplica por 1,10,y 100 respectivamente
                 $f *= 10;
             }
 
-            if ( $mm == 1 )
-            {
+            if ($mm == 1) {
                 $this->millones = $this->millon;
             }
             $millones_txt = $this->menor_mil($mm);
@@ -186,9 +180,8 @@ class Nat {
             $f=1;
             $m = 0;
             //Separa los miles para ser evaluados con la función menor_mil
-            for ($k = $long_n; $k > $long_n - 3; $k--)
-            {
-                $m += ($n_str[$k-4] * $f);	//Multiplica por 1,10,y 100
+            for ($k = $long_n; $k > $long_n - 3; $k--) {
+                $m += ($n_str[$k-4] * $f);    //Multiplica por 1,10,y 100
                 $f *= 10;
             }
 
@@ -196,23 +189,21 @@ class Nat {
             $miles_txt = $this->menor_mil($m);
             $n = $this->numero_entero - $m*1000 - $mm*1000000;//Elimina los miles y millones
             $n_txt = $this->menor_mil($n);//Unidades, Decenas y Centenas a ser evaluadas
-            $letras = $millones_txt.$this->millones.$miles_txt.$this->miles.$n_txt." ".$this->moneda;
+            $letras = $millones_txt . $this->millones . $miles_txt . $this->miles . $n_txt . " " . $this->moneda;
         }
 
         //Evaluación de decimales
         //Si no tiene decimal
-        if ( strchr($this->numero,".") == "" )
-        {
-            return ("(".$letras." 00/100)");
+        if (strchr($this->numero, ".") == "") {
+            return ("(" . $letras . " 00/100)");
         } else {
-            $dec = strchr($this->numero,".");
-            if(empty($dec[2])) {
-                $d = $dec['1'].'0';
+            $dec = strchr($this->numero, ".");
+            if (empty($dec[2])) {
+                $d = $dec['1'] . '0';
             } else {
-                $d = $dec['1'].(integer)$dec['2'];
+                $d = $dec['1'] . (integer)$dec['2'];
             }
-            return ("(".$letras." ".$d."/100)");
+            return ("(" . $letras . " " . $d . "/100)");
         }
-
     }
 }
